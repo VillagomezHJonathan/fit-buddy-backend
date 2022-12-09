3ae8a74eac70d5db7bc184b279c4edb1a191eb93
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-12-09 13:21:59 -0500

    Executed exercise table migration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models.db import db
+from models.exercise import Exercise
 
 app = Flask(__name__)
 CORS(app)
