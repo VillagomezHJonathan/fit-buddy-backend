32bbbf8fc8bf4226a9e22f6b02bf57e5b7e6a8ed
Jonathan Villagomez <villagomezhjonathan@gmail.com>
2022-12-19 13:27:03 -0500

    Prepped for deployment

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 from models.db import db
 from models.exercise import Exercise
 from models.user import User
@@ -16,13 +18,25 @@ from resources.exercise_res import Exercises
 from resources.day_exercise_res import DayExercises
 from middleware import strip_token, read_token
 
+load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/fitbuddy_db"
-app.config['SQLALCHEMY_ECHO'] = True
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace(
+        "://", "ql://", 1)
+    app.config['SQLALCHEMY_ECHO'] = False
+    app.env = 'production'
+else:
+    app.debug = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/fitbuddy_db"
+    app.config['SQLALCHEMY_ECHO'] = True
+
 
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -45,4 +59,4 @@ api.add_resource(Exercises, '/api/exercises')
 api.add_resource(DayExercises, '/api/days-exercises')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
